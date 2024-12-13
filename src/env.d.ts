cb4f63bf166f7aaf35f14ee5f1a7a44fc26d0a74
ianlp <ian@westtownwebsites.com>
2024-12-13 11:30:54 -0600

    init

--- src/env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-interface Window {
-  gsap: typeof import("gsap");
-}
--- vite.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  optimizeDeps: {
-    include: ["gsap"],
-  },
-});
